064ba8f84146bb2b7d8bc450ecf16e550029a9c2
Ibrahim <ihussein456@gmail.com>
2025-04-01 11:43:07 +0100

    adding frontend

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ import express from 'express';
 import multer from 'multer';
 import { handleUpload, checkStatus } from './controllers/uploadController.js';
 import { limiter } from './middleware/rateLimiter.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, '../public')));
+
 
 const app = express();
 const upload = multer({ dest: 'uploads/' });
